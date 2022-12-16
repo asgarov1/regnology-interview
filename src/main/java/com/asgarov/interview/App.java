1fd6d5a0eab13b4be3df129f6beca98fd1c55bc9
Javid Asgarov <asgarov1@gmail.com>
2022-12-16 16:03:31 +0100

    initial commit

---
+++ src/main/java/com/asgarov/interview/App.java
@@ -0,0 +1,10 @@
+package com.asgarov.interview;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.version"));
+    }
+}
---
+++ src/test/java/com/asgarov/interview/AppTest.java
@@ -0,0 +1,19 @@
+package com.asgarov.interview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(2 * 2 == 4);
+        assertTrue(2 * 2 == 5);
+    }
+}
